0a768839ddfce94ce44cf499a8544aa19196e972
Bimbola Lasisi <78049244+blasisi@users.noreply.github.com>
2021-09-28 12:56:11 +0100

    exercise 3 completed

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,14 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+let total = 0;
+console.log(`Qty   item                     Total`);
+order.forEach((list)=> {
+const {itemName, quantity, unitPrice} =list;
+  console.log(`${quantity}     ${itemName}              ${unitPrice}`);
+  total = total+unitPrice;
+});
+
+
+console.log(`Total is ${total}`);
